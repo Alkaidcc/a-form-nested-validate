6b5d482a72d7ebaced865431e74737e74a27a582
Alkaidcc <alkaidcc@qq.com>
2023-09-09 14:13:26 +0800

    chore: init project

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const info = ref({
+  name: '',
+  person: {
+    sex: ''
+  }
+})
+</script>
+
+<template>
+  <a-form :model="info">
+    <a-form-item name="name" :rules="[{required: true,message: 'name required',trigger: 'blur'}]">
+      <a-input v-model:value="info.name"></a-input>
+    </a-form-item>
+    <a-form-item name="person.sex" :rules="[{required: true,message: 'sex required',trigger: 'blur'}]">
+      <a-input v-model:value="info.person.sex"/>
+    </a-form-item>
+  </a-form>
+</template>
+
